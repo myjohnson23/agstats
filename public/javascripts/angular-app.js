568c3a66fbaeb593476bcce8801eb4cd103a5ba9
myjohnson23 <myjohnson@gmail.com>
2015-09-15 15:45:13 -0500

    Got Angular and D3 directives working and display table and chart

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -1,86 +1,103 @@
-// Create Angular App namespace
-var agStatsApp = angular.module('agStats', []);
+// Init App
+angular.module('agStats', []);
 
-agStatsApp.controller('agStatsCtrl', ["$scope", "d3", "SimpleHttpLoader", function($scope, d3, SimpleHttpLoader) {
+// The factory
+angular.module('agStats').factory('d3', function() {
+      return d3;
+    });
+
+angular.module('agStats').factory('SimpleHttpLoader', ["$http", function($http) {
+      return function(url) {
+        return $http.get(url);
+      }
+    }]);
+
+// Controller
+angular.module('agStats').controller('agStatsCtrl', ["$scope", "d3", "SimpleHttpLoader", function($scope, d3, SimpleHttpLoader) {
     $scope.players = {
       src: '/api',
-      // data: [{playerID: "Myron", HR: 5, H: 10, RBI: 112}]
       data: []
     };
     SimpleHttpLoader($scope.players.src).then(function(response) {
       $scope.players.data = response.data;
       console.log($scope.players);
+      //This below is how I was able to access the data
+      console.log($scope.players.data[0].HR);
     });
-
   }]);
 
-agStatsApp
-    // D3 Factory
-    .factory('d3', function() {
-      return d3;
-    })
-
-    .directive('myScatterChart', ["d3", function(d3) {
+// Directives
+angular.module('agStats').directive('myScatterChart', ["d3", function(d3) {
 
       function draw(svg, width, height, data) {
+        console.log('first');
+        console.log(data);
        svg
          .attr('width', width)
          .attr('height', height);
 
+         console.log("inside-draw");
+         console.log(data.length);
+         for (var i = 0; i < data.length; i++) {
+           for(var ind in data[i]) {
+              console.log(ind);
+              for(var vals in data[i][ind]) {
+                console.log(vals, data[i][ind][vals]);
+              }
+           }
+         };
        // Define a margin
        var margin = 30;
 
        // Define x scale
        var xScale = d3.scale.linear()
-         .domain([0, d3.max(data, function(d) { return d.x; })])
+         .domain([0, d3.max(data, function(d) { return d.HR; })])
          .range([margin, width-margin]);
 
        // Define x-axis
-       var xAxis = d3.svg.axis()
-         .scale(xScale)
-         .orient('top')
-         .tickFormat(d3.time.format('%S'));
+      //  var xAxis = d3.svg.axis()
+      //    .scale(xScale)
+      //    .orient('top')
+      //    .tickFormat(d3.time.format('%S'));
 
        // Define y-scale
        var yScale = d3.scale.linear()
-         .domain([0, d3.max(data, function(d) { return d.y; })])
+         .domain([0, d3.max(data, function(d) { return d.AB; })])
          .range([margin, height-margin]);
 
        // Define y-axis
-       var yAxis = d3.svg.axis()
-         .scale(yScale)
-         .orient('left')
-         .tickFormat(d3.format('f'));
+      //  var yAxis = d3.svg.axis()
+      //    .scale(yScale)
+      //    .orient('left')
+      //    .tickFormat(d3.format('f'));
 
        // Draw the x-axis
-       svg.select('.x-axis')
-         .attr("transform", "translate(0, " + margin + ")")
-         .call(xAxis);
+      //  svg.select('.x-axis')
+      //    .attr("transform", "translate(0, " + margin + ")")
+      //    .call(xAxis);
 
        // Draw the y-axis
-       svg.select('.y-axis')
-         .attr("transform", "translate(" + margin + ")")
-         .call(yAxis);
+      //  svg.select('.y-axis')
+      //    .attr("transform", "translate(" + margin + ")")
+      //    .call(yAxis);
 
        // Add new the data points
        svg.select('.data')
          .selectAll('circle').data(data)
          .enter()
          .append('circle');
-         console.log(data);
+
        // Updated all data points
        svg.select('.data')
          .selectAll('circle').data(data)
          .attr('r', 2.5)
-         .attr('cx', function(d) { return xScale(d.x); })
-         .attr('cy', function(d) { return yScale(d.y); });
-     };
-
+         .attr('cx', function(d) { return xScale(d.HR); })
+         .attr('cy', function(d) { return yScale(d.AB); });
+     }
       return {
-        restrict: 'EA'
-        },
+        restrict: 'EA',
 
-        compile = function( element, attrs, transclude ) {
+        compile: function( element, attrs, transclude ) {
           var svg  = d3.select(element[0]).append('svg');
           svg.append('g').attr('class', 'data');
           svg.append('g').attr('class', 'x-axis axis');
@@ -90,27 +107,24 @@ agStatsApp
 
           return function(scope, element, attrs) {
 
-            scope.$watch('data', function(newVal, oldVal, scope) {
-              console.log(scope);
-              var data = scope.players.data.map(function(d) {
-                return {
-                  x: d.HR,
-                  y: d.H
-                }
-              });
-              draw(svg, width, height, data);
+            scope.$watch(function(scope) {
+              console.log(scope.players.data);
+              return scope.players.data },
+
+              function(newVal, oldVal, scope) {
+              console.log("$watch");
+              // var data = scope.players.map(function(d) {
+              //   return {
+              //     x: d.HR,
+              //     y: d.H
+              //   }
+              // });
               if (scope.players.data) {
-
+                  draw(svg, width, height, scope.players.data);
               }
-
 
             }, true);
           };
         }
       }
-    ])
-    .factory('SimpleHttpLoader', ["$http", function($http) {
-      return function(url) {
-        return $http.get(url);
-      }
-    }]);
+    }])
