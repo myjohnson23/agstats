e1fc358a2c070b5e66fb841d4eb2b6242169a85d
myjohnson23 <myjohnson@gmail.com>
2015-09-14 22:25:49 -0500

    Reverting back to clean Angular state

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -1,8 +1,51 @@
 // Create Angular App namespace
-var agStatsApp = angular.module('agStats', []);
+var agStatsApp = angular.module('agStats', ['nvd3']);
 
 agStatsApp.controller('agStatsCtrl', function($scope, $http) {
   $http.get('/api').success(function(data) {
+    // Chart options
+    $scope.options = {
+    chart: {
+        type: 'discreteBarChart',
+        height: 450,
+        margin : {
+            top: 20,
+            right: 20,
+            bottom: 60,
+            left: 55
+        },
+        x: function(d){ return d.HR; },
+        y: function(d){ return d.RBI; },
+        showValues: true,
+        valueFormat: function(d){
+            return d3.format(',.4f')(d);
+        },
+        transitionDuration: 500,
+        xAxis: {
+            axisLabel: 'X Axis'
+        },
+        yAxis: {
+            axisLabel: 'Y Axis',
+            axisLabelDistance: 30
+        }
+    }
+};
+    // Chart data
+    // $scope.data = data;
+    $scope.data = [
+      {"HR": "5"},
+      {""}
+    ]
+  });
+});
+
+Commented out for testing above
+agStatsApp.controller('agStatsCtrl', function($scope, $http) {
+  $http.get('/api').success(function(data) {
+    console.log(data);
+    console.log(data[0]);
+    console.log(data[0].HR);
+    tempData = data;
     $scope.players = data;
   });
 });
@@ -14,15 +57,14 @@ agStatsApp
       replace: false,
       // scope: {data: '=chartData'},
       link: function(scope, element, attrs) {
-        var data = attrs.chartData.split(',');
-        var chart = d3.select(element[0]);
+        var data = attrs.tempData.split(',');
+        var chart = d3.select(".test");
         chart.append("div").attr("class", "chart")
           .selectAll('div')
           .data(data).enter().append('div')
           .transition().ease("elastic")
-          .style("width", "10px")
-          // .style("width", function(d, i) { return d[i].HR + "%"; })
-          // .text(function(d, i) { return d[i].HR + "%"; });
+          .style("width", function(d) { return d.HR + "%"; })
+          .text(function(d) { return d.HR + "%"; });
         }
       };
       return directiveDefinitionObject;
